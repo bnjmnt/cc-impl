2147bfce4dc3b1bc34f40ab684835f0e6cfa60f4
Benjamin Tam <benjamint1205@gmail.com>
2026-09-06 16:48:45 -0400

    feat(unique_ptr): implement rule of 5

---
+++ unique_ptr/unique_ptr.h
@@ -0,0 +1,40 @@
+#ifndef CC_IMPL_UNIQUE_PTR_UNIQUE_PTR_H
+#define CC_IMPL_UNIQUE_PTR_UNIQUE_PTR_H
+
+#include <utility>
+
+namespace ben {
+
+template <typename T>
+class unique_ptr {
+ public:
+  unique_ptr(T* ptr = nullptr) noexcept : ptr_(ptr) {}
+
+  ~unique_ptr() { delete ptr_; }
+
+  unique_ptr(const unique_ptr&) noexcept = delete;
+
+  unique_ptr& operator=(const unique_ptr&) noexcept = delete;
+
+  unique_ptr(unique_ptr&& other) noexcept {
+    ptr_ = std::exchange(other.ptr_, nullptr);
+  }
+
+  unique_ptr& operator=(unique_ptr&& other) noexcept {
+    if (this == &other) {
+      return *this;
+    }
+    delete ptr_;
+    ptr_ = std::exchange(other.ptr_, nullptr);
+    return *this;
+  }
+
+  T* get() const noexcept { return ptr_; }
+
+ private:
+  T* ptr_;
+};
+
+}  // namespace ben
+
+#endif  // CC_IMPL_UNIQUE_PTR_UNIQUE_PTR_H
